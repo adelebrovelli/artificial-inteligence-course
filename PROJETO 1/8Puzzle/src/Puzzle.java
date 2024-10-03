4b658bfe7d9213c7cb09be0413625d8bdddd0c45
adelebrovelli <borba.adele5@gmail.com>
2024-10-03 12:00:50 -0300

    quase lá

--- PROJETO 1/8Puzzle/src/Puzzle.java
+++ PROJETO 1/8Puzzle/src/Puzzle.java
@@ -20,28 +20,61 @@ public class Puzzle {
         for (int i = 0; i < 3; i++) { 
             for (int j = 0; j < 3; j++) {
                 initialState.getState()[i][j] = goalState.getState()[i][j]; //agora initial tá igual ao objetivo
-            }
+            } 
         }
-        //eu quero que embaralhe 10 vezes os numeros
-        for(int i = 0; i < 10;i++){
+        //eu quero que embaralhe 10 vezes os numeros e seja aleatorio
+        for(int i = 0;i<10;i++) {
+
             int i1 = random.nextInt(3);
             int j1 = random.nextInt(3);
             int i2 = random.nextInt(3);
             int j2 = random.nextInt(3);
-    
-            int keepVar = initialState.getState()[i1][j1];
-            initialState.getState()[i1][j1] = initialState.getState()[i2][j2];
-            initialState.getState()[i2][j2] = keepVar;
+            
+            initialState = troca(i1, j1, i2, j2, initialState);
+        
         }
        }
 
+       public PuzzleNode troca(int i1, int j1, int i2, int j2, PuzzleNode actualState) {
+            int keepVar = actualState.getState()[i1][j1];
+            actualState.getState()[i1][j1] = actualState.getState()[i2][j2];
+            actualState.getState()[i2][j2] = keepVar;
+        return actualState;
+       }
+
+       public boolean verification() {
+        int[] matrizLinear = new int[9]; // Linearizamos a matriz para um array de tamanho 9
+        int iAux = 0;
+        
+        // Linearizar a matriz (de 3x3 para 1x9)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrizLinear[iAux] = initialState.getState()[i][j];
+                iAux++;
+            }
+        }
+    
+        // Contar inversões
+        int cont = 0;
+        for (int i = 0; i < matrizLinear.length; i++) {
+            for (int j = i + 1; j < matrizLinear.length; j++) {
+                if (matrizLinear[i] != 0 && matrizLinear[j] != 0 && matrizLinear[i] > matrizLinear[j]) {
+                    cont++;
+                }
+            }
+        }
+    
+        return cont % 2 == 0; //vai retornar true se for par
+    }
+
        public PuzzleNode evokeIDS (PuzzleNode actualState){
-        while (actualState.depth>l) {
-            actualState = actualState.getPointerNode();
-           // if (actualState.getState().equals(goalState.getState()) == false){
-            //    actualState.setDepth(+1);
-            //    l++;
-         //   }    
+        while (actualState.depth<l) { 
+            PuzzleNode result = IDS(actualState, l);
+    
+            if (result != null) {
+                return result; 
+            }
+            l++; 
         }
         return actualState;
        }
@@ -53,7 +86,7 @@ public class Puzzle {
         while(!frontier.isEmpty()){ //enquanto eu n esvazio a fronteira
             PuzzleNode NoTopoPilha = frontier.pop();
 
-            if (NoTopoPilha.equals(goalState) ){
+            if (NoTopoPilha.equals(goalState)){
                 return NoTopoPilha; //achou o nó que tava no topo da pilha, aí quebra o while
                 } 
 
@@ -70,13 +103,84 @@ public class Puzzle {
         return null; //buscou, colocou tudo na fronteira, buscou mais e ainda assim nao achou
        }
 
-       public PuzzleNode[] acoesPossiveis(PuzzleNode actualState){
-        //aqui ele vai pegar o no atual, analisar qual a posicao que o 0 se encontra na matriz
-        // e a partir disso, analisar se as possibilidades são de gerar 2,3 ou 4 filhos, e ai armazenar em um vetor e retornar ele
-        //aí ainda tem, se a possibilidade for 2: ai ve os mais proximos do 2 sei la ou entao analisa
-        // caso por caso pra dizer quais das acoes(cima baixo lado lado) pode ser
-        return null;
-       }
+       public PuzzleNode[] acoesPossiveis(PuzzleNode actualState) {
+        PuzzleNode[] vetorDePossibilidades = new PuzzleNode[2];
+        PuzzleNode[] vetorDePossibilidades1 = new PuzzleNode[3];
+        PuzzleNode[] vetorDePossibilidades2 = new PuzzleNode[4];
+    
+        if (actualState.getState()[0][0] == 0) { // canto superior esquerdo
+            switch (0) {
+                case 0: // canto superior esquerdo
+                    vetorDePossibilidades[0] = troca(0, 0, 0, 1, actualState); // direita
+                    vetorDePossibilidades[1] = troca(0, 0, 1, 0, actualState); // baixo
+                    return vetorDePossibilidades;
+            }
+        } else if (actualState.getState()[0][2] == 0) { // canto superior direito
+            switch (2) {
+                case 2: // canto superior direito
+                    vetorDePossibilidades[0] = troca(0, 2, 0, 1, actualState); // esquerda
+                    vetorDePossibilidades[1] = troca(0, 2, 1, 2, actualState); // baixo
+                    return vetorDePossibilidades;
+            }
+        } else if (actualState.getState()[2][0] == 0) { // canto inferior esquerdo
+            switch (6) {
+                case 6: // canto inferior esquerdo
+                    vetorDePossibilidades[0] = troca(2, 0, 1, 0, actualState); // cima
+                    vetorDePossibilidades[1] = troca(2, 0, 2, 1, actualState); // direita
+                    return vetorDePossibilidades;
+            }
+        } else if (actualState.getState()[2][2] == 0) { // canto inferior direito
+            switch (8) {
+                case 8: // canto inferior direito
+                    vetorDePossibilidades[0] = troca(2, 2, 1, 2, actualState); // cima
+                    vetorDePossibilidades[1] = troca(2, 2, 2, 1, actualState); // esquerda
+                    return vetorDePossibilidades;
+            }
+        } else if (actualState.getState()[0][1] == 0) { // meio superior
+            switch (1) {
+                case 1:
+                    vetorDePossibilidades1[0] = troca(0, 1, 0, 0, actualState); // esquerda
+                    vetorDePossibilidades1[1] = troca(0, 1, 1, 1, actualState); // baixo
+                    vetorDePossibilidades1[2] = troca(0, 1, 0, 2, actualState); // direita
+                    return vetorDePossibilidades1;
+            }
+        } else if (actualState.getState()[1][0] == 0) { // meio esquerdo
+            switch (3) {
+                case 3:
+                    vetorDePossibilidades1[0] = troca(1, 0, 0, 0, actualState); // cima
+                    vetorDePossibilidades1[1] = troca(1, 0, 1, 1, actualState); // direita
+                    vetorDePossibilidades1[2] = troca(1, 0, 2, 0, actualState); // baixo
+                    return vetorDePossibilidades1;
+            }
+        } else if (actualState.getState()[1][2] == 0) { // meio direito
+            switch (5) {
+                case 5:
+                    vetorDePossibilidades1[0] = troca(1, 2, 0, 2, actualState); // cima
+                    vetorDePossibilidades1[1] = troca(1, 2, 1, 1, actualState); // esquerda
+                    vetorDePossibilidades1[2] = troca(1, 2, 2, 2, actualState); // baixo
+                    return vetorDePossibilidades1;
+            }
+        } else if (actualState.getState()[2][1] == 0) { // meio inferior
+            switch (7) {
+                case 7:
+                    vetorDePossibilidades1[0] = troca(2, 1, 2, 0, actualState); // esquerda
+                    vetorDePossibilidades1[1] = troca(2, 1, 1, 1, actualState); // cima
+                    vetorDePossibilidades1[2] = troca(2, 1, 2, 2, actualState); // direita
+                    return vetorDePossibilidades1;
+            }
+        } else if (actualState.getState()[1][1] == 0) { // centro
+            switch (4) {
+                case 4:
+                    vetorDePossibilidades2[0] = troca(1, 1, 0, 1, actualState); // cima
+                    vetorDePossibilidades2[1] = troca(1, 1, 1, 0, actualState); // esquerda
+                    vetorDePossibilidades2[2] = troca(1, 1, 2, 1, actualState); // baixo
+                    vetorDePossibilidades2[3] = troca(1, 1, 1, 2, actualState); // direita
+                    return vetorDePossibilidades2;
+            }
+        }
+        return null; // caso nenhum zero seja encontrado
+    }
+    
     
         public void printState(int[][] state) {
             for (int i = 0; i < state.length; i++) {
@@ -89,7 +193,24 @@ public class Puzzle {
     
         public static void main(String[] args) throws Exception {
             Puzzle puzzle = new Puzzle();
-            puzzle.randomizeInitialState();
+        boolean ver = puzzle.verification();
+
+        if (ver) {
+            System.out.println("Estado inicial embaralhado:");
             puzzle.printState(puzzle.initialState.getState());
+            
+            System.out.println("\nBuscando solução...");
+            PuzzleNode solvedNode = puzzle.evokeIDS(puzzle.initialState);
+
+            if (solvedNode != null && solvedNode.equals(puzzle.goalState)) {
+                System.out.println("\nSolução encontrada:");
+                puzzle.printState(solvedNode.getState());
+            } else {
+                System.out.println("\nNão foi possível encontrar uma solução.");
+            }
+
+        } else {
+            System.out.println("Desculpe, estado inicial não solucionável, reinicie.");
+        }
         }
 }
--- PROJETO 1/8Puzzle/src/PuzzleNode.java
+++ PROJETO 1/8Puzzle/src/PuzzleNode.java
@@ -1,61 +1,29 @@
-import java.util.Random;
 import java.util.Arrays; 
 
 public class PuzzleNode {
 // esta classe seria equivalente aos nós do puzzle, logo, os estados.
 int[][] state;
-PuzzleNode pointerNode; //nó pai
-int depth = 1;
+int depth;
 
 public PuzzleNode(int[][] state) {
     this.state = state;
-    this.pointerNode = pointerNode;
-    this.depth = depth;
+    this.depth = 1; // profundidade inicial como 1
 }
 
 public int[][] getState() {
     return state;
 }
 
-
-
 public void setState(int[][] state) {
     this.state = state;
 }
-
-
-
-public PuzzleNode getPointerNode() {
-    return pointerNode;
-}
-
-
-
-public void setPointerNode(PuzzleNode pointerNode) {
-    this.pointerNode = pointerNode;
-}
-
-
 
 public int getDepth() {
     return depth;
 }
 
-
-
 public void setDepth(int depth) {
     this.depth = depth;
-}
-
-
-
-public void printState(int[][] state) {
-    for (int i = 0; i < state.length; i++) {
-        for (int j = 0; j < state[i].length; j++) {
-            System.out.print(state[i][j] + " ");
-        }
-        System.out.println(); 
-    }
 }
 
 @Override
@@ -70,6 +38,4 @@ public boolean equals(Object obj) {
     // Comparar os arrays (uso de Arrays.deepEquals para comparar o conteúdo das matrizes)
     return Arrays.deepEquals(this.state, other.state);
 }
-
-   
 }
